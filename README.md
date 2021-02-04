1e1c280c4a6c6980857f3d378452bbf630fc72c2
chimurai <655241+chimurai@users.noreply.github.com>
2021-02-04 16:04:25 +0100

    feat(install.sh): add installation script

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ dotfiles, managed with [`chezmoi`](https://github.com/twpayne/chezmoi).
 
 ## Install
 
+This'll install Homebrew and chezmoi. After installation `chezmoi` will be initialized.
+
+```shell
+curl -fsSL https://raw.githubusercontent.com/chimurai/dotfiles/master/install.sh | sh -s
+```
+
+## Manual install
+
+Prerequisite: Homebrew & chezmoi
+
 ```shell
 # Setup
 chezmoi init https://github.com/chimurai/dotfiles.git
---
+++ install.sh
@@ -0,0 +1,23 @@
+set -eufo pipefail
+
+echo ""
+echo "🤚  This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
+
+
+# Install Homebrew
+command -v brew >/dev/null 2>&1 || \
+  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Install chezmoi
+brew install chezmoi || true
+
+if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+  echo "🚸  chezmoi already initialized"
+  echo "    Reinitialize with: 'chezmoi init https://github.com/chimurai/dotfiles.git'"
+else
+  echo "🔧  chezmoi init"
+  # chezmoi init https://github.com/chimurai/dotfiles.git
+fi
+
+echo "\nDone."
