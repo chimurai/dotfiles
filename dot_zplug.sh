9b57010b9cc25e2939573d028f54d2329f67fd01
chimurai <655241+chimurai@users.noreply.github.com>
2021-02-05 19:12:32 +0100

    feat(zplug): manage plugins with zplug

---
+++ dot_zplug.sh
@@ -0,0 +1,14 @@
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+zplug "plugins/nvm", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/autojump", from:oh-my-zsh
+
+zplug "bigH/git-fuzzy", as:command, use:"bin/git-fuzzy"
+
+zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
+zplug "zdharma/fast-syntax-highlighting", as:plugin, defer:2
+zplug "zsh-users/zsh-autosuggestions", as:plugin, defer:2
+
+zplug load
---
+++ run_once_00_clean_up.sh.tmpl
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+# Uninstall old dependencies
+brew uninstall zsh-autosuggestions >/dev/null 2>&1 || true
+brew uninstall zsh-syntax-highlighting >/dev/null 2>&1 || true
